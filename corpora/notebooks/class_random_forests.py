aa8d56496a8780a1328592aebb01db6a053a2f21
tostre <marcel.mueller3101@gmail.com>
2019-11-18 22:58:16 +0100

    f

--- corpora/notebooks/class_random_forests.py
+++ corpora/notebooks/class_random_forests.py
@@ -275,18 +275,26 @@ types = {
 # In[119]:
 
 for dataset in ["norm_tweet", "norm_emotion"]:
-		# lex data
-		for key, feature_set in features.items(): 
-		    print("--------------------", dataset, feature_set)
-		    results, importance = classify_with_rf(dataset, key, *load_lex_data(dataset, feature_set), trees_for_dataset.get(dataset_name + "_" + key, 10))
-		    all_results.append([dataset, key, *results])
-		    importances.append(importance)
+		# unigram dataset
+		print("--------------------", dataset, "vec-unigram")
+		results, coef = classify_with_rf(dataset, "vec-unigram", *load_vector_data(dataset), trees_for_dataset.get(dataset + "_" + key, 10))
+		all_results.append([dataset, "vec-unigram", *results])
+		importances.append(coef)
+		# bigram dataset
+		print("--------------------", dataset, "vec-bigram")
+		results, coef = classify_with_rf(dataset,  "vec-bigram", *load_vector_data(dataset, True), trees_for_dataset.get(dataset + "_" + key, 10))
+		all_results.append([dataset, "vec-bigram", *results])
+		importances.append(coef)
+		# topic dataset
+		print("--------------------", dataset, "topics-unigram")
+		results, coef = classify_with_rf(dataset, "topics", *load_topic_data(dataset, num_topics_dict[dataset]), trees_for_dataset.get(dataset + "_" + key, 10))
+		all_results.append([dataset, "topics", *results])
+		importances.append(coef)
 for index, result in enumerate(all_results):
 		with open("../reports/report_rf_" + result[0] + "_"  + result[1] + ".txt", 'w') as f:
 		    print((result[0] + "_" + result[1] + " (" + str(result[5]) + "):\n" + 
 		      classification_report(result[2], result[3],target_names=classes)), file=f)
 		draw_confusion_matrix(*result)
-		draw_importances(result[0], result[1], features[result[1]], importances[index])
 
 
 all_results = []
