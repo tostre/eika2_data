4e87d22b4fba1c6d7ea5c97c4cb76746a80ee9f7
tostre <marcel.mueller3101@gmail.com>
2019-11-20 00:58:25 +0100

    finished implementing lin net

--- corpora/notebooks/class_net_lin.py
+++ corpora/notebooks/class_net_lin.py
@@ -46,13 +46,16 @@ class Lin_Net(nn.Module):
         super(Lin_Net, self).__init__()
         self.act_function = nn.ReLU()
         self.lin1 = nn.Linear(input_dim, hidden_dim)
-        self.hidden_layers = [nn.Linear(hidden_dim, hidden_dim) for i in range(0, num_hidden_layers)]
+        #self.hidden_layers = [nn.Linear(hidden_dim, hidden_dim) for i in range(0, num_hidden_layers)]
+        self.hidden_layers = nn.ModuleList([nn.Linear(hidden_dim, hidden_dim) for i in range(0, num_hidden_layers)])
         self.lin4 = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
         x = self.act_function(self.lin1(x))
-        for hidden_layer in self.hidden_layers: 
+        for i, hidden_layer in enumerate(self.hidden_layers):
             x = self.act_function(hidden_layer(x))
+        #for hidden_layer in self.hidden_layers: 
+        #    x = self.act_function(hidden_layer(x))
         x = self.lin4(x)
         return x
     
